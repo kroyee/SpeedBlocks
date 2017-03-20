209899b30e8b7a40636bdd817a37b9ad1775ab81
ZyphDoz <zyphdoz@users.noreply.github.com>
2017-03-20 17:28:26 +0100

    Changed the dark blue piece color

--- textures.cpp
+++ textures.cpp
@@ -13,7 +13,7 @@ void textures::loadTextures() {
 
     tile[0].setTexture(tileTexture); tile[0].setColor(sf::Color(255,0,0));
     tile[1].setTexture(tileTexture); tile[1].setColor(sf::Color(0,255,0));
-    tile[2].setTexture(tileTexture); tile[2].setColor(sf::Color(30,30,255));
+    tile[2].setTexture(tileTexture); tile[2].setColor(sf::Color(115,145,255));
     tile[3].setTexture(tileTexture); tile[3].setColor(sf::Color(255,0,255));
     tile[4].setTexture(tileTexture); tile[4].setColor(sf::Color(0,255,255));
     tile[5].setTexture(tileTexture); tile[5].setColor(sf::Color(255,255,0));
@@ -22,7 +22,7 @@ void textures::loadTextures() {
 
     tile[8].setTexture(tileTexture); tile[8].setColor(sf::Color(255,0,0,120));
     tile[9].setTexture(tileTexture); tile[9].setColor(sf::Color(0,255,0,120));
-    tile[10].setTexture(tileTexture); tile[10].setColor(sf::Color(30,30,255,120));
+    tile[10].setTexture(tileTexture); tile[10].setColor(sf::Color(115,145,255,120));
     tile[11].setTexture(tileTexture); tile[11].setColor(sf::Color(255,0,255,120));
     tile[12].setTexture(tileTexture); tile[12].setColor(sf::Color(0,255,255,120));
     tile[13].setTexture(tileTexture); tile[13].setColor(sf::Color(255,255,0,120));
