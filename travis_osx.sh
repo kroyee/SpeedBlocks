5d78b3a5785bd3449d10e24b2698c0caab21f414
kroyee <olle.samuelsson@gmail.com>
2019-01-02 18:28:02 +0100

    fixed tgui version naming for travis

--- travis_osx.sh
+++ travis_osx.sh
@@ -17,13 +17,13 @@ else
   echo "$(tput setaf 2)Using cached SFML directory$(tput sgr 0)"
 fi
 
-export TGUI_VERSION=v0.8.2
+export TGUI_VERSION=0.8.2
 export TGUI_ROOT=$HOME/TGUI_OSX
 
 if [[ ! -d "$TGUI_ROOT/lib" ]]; then
   echo "$(tput setaf 3)Rebuilding TGUI: no cache available$(tput sgr 0)"
 
-  wget -O TGUI.tar.gz https://github.com/texus/TGUI/archive/${TGUI_VERSION}.tar.gz
+  wget -O TGUI.tar.gz https://github.com/texus/TGUI/archive/v${TGUI_VERSION}.tar.gz
   tar -xzf TGUI.tar.gz
   cd TGUI-${TGUI_VERSION}
   cmake -DCMAKE_INSTALL_PREFIX=$TGUI_ROOT -DCMAKE_INSTALL_FRAMEWORK_PREFIX=$TGUI_ROOT/lib -DTGUI_BUILD_FRAMEWORKS=TRUE -DSFML_INCLUDE_DIR=$SFML_ROOT/lib/SFML.framework -DTGUI_OPTIMIZE_SINGLE_BUILD=TRUE .
